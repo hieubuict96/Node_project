0bd84c70e7e4cc93851709ef13d96d2431c0120b
hieubuict96 <hieubuict96@gmail.com>
2020-09-27 22:26:56 +0700

    Fd

---
+++ Bai_32/app.js
@@ -0,0 +1,9 @@
+var person = {
+    name: "Hiếu",
+    age: 20
+}
+
+console.log(`Tên tôi là ${person.name}, năm nay ${person.age}
+
+ tuổi`);
+
--- index3.js
+++ index3.js
@@ -1,7 +1,11 @@
-function person() {
-    this.name = "hieu";
-    this.age = 25;
+var obj = {
+    name: "Mai Hoa", 
+    sayHello: function (param1, param2) {
+    console.log(`Hello ${ this.name }`);
+    console.log("Params: ", param1, param2);
+    }
 }
 
-module.exports = person;
-
+obj.sayHello("Xin chào", "2016");
+obj.sayHello.call({ name: "Yến Phượng" }, "Xin chào", "2017");
+obj.sayHello.apply({ name: "Yến Phượng" }, ["Xin chào", "2018"]);
