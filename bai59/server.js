e0344e9ad7b791cc066bff40da03b4d3633d1c40
hieubuict96 <hieubuict96@gmail.com>
2020-10-07 15:09:53 +0700

    fd

--- bai59/server.js
+++ bai59/server.js
@@ -11,10 +11,13 @@ http.createServer(function (req, res) {
     if (req.url === "/") {
         res.writeHead(200, { "Content-Type": "text/plain" });
         res.end("url success")
+    } else if (req.url === "/index.html") {
+        res.writeHead(200, { "Content-Type": "text/plain" });
+        res.end("Truy cap index.html")
     } else {
         res.writeHead(404)
         res.end("Not Found")
     }
-}).listen(1335, "127.0.2.1", function () {
+}).listen(1335, "127.0.0.1", function () {
     console.log("Thiet bi duoc nghe tai port: 1335");
 });
