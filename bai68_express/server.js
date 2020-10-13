8fdfd8bcca1f25d191a1436328bde282f85d3825
hieubuict96 <hieubuict96@gmail.com>
2020-10-13 09:17:38 +0700

    fds

---
+++ bai68_express/server.js
@@ -0,0 +1,9 @@
+var express = require("express")
+var app = express();
+app.listen(3000, function () {
+    console.log("Server is listening port: ", 3000);
+})
+
+app.get("/", function (req, res) {
+    res.send("fsdfsfd")
+})
